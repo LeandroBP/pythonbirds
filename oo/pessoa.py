4116b5a8fa33b6b2a3d63fc8782558834bf70521
Leandro B Pereira <leandrobatistapereira98@gmail.com>
2020-03-07 13:51:25 -0300

    Criados atributos complexo filhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,16 +1,20 @@
 class Pessoa:
-    def __init__(self, nome=None, idade=35):
+    def __init__(self, *filhos, nome=None, idade=35):
         self.idade = idade
         self.nome = nome
+        self.filhos = list(filhos)
 
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 
 if __name__ == '__main__':
-    p = Pessoa('Tiago')
-    print(Pessoa.cumprimentar(p))
-    print(p.cumprimentar())
-    print(id(p))
-    print(p.nome)
-    print(p.idade)
+    Leandro = Pessoa(nome='Leandro')
+    Tiago = Pessoa(Leandro, nome='Tiago')
+    print(Pessoa.cumprimentar(Tiago))
+    print(Tiago.cumprimentar())
+    print(id(Tiago))
+    print(Tiago.nome)
+    print(Tiago.idade)
+    for filho in Tiago.filhos:
+        print(filho.nome)
